2be150c8499d08ce3396a91376b1a8d4f7313edb
michalnand <michal.nand@gmail.com>
2018-12-30 11:59:59 +0100

    nets

--- libs/dataset_detection.cpp
+++ libs/dataset_detection.cpp
@@ -15,14 +15,12 @@ DatasetDetection::DatasetDetection(std::string json_file_name)
 
     float training_testing_ratio = json.result["training testing ratio"].asFloat();
 
-    float threshold   = json.result["threshold"].asFloat();
-    float alpha       = json.result["alpha"].asFloat();
-    float background_inversion = json.result["background inversion"].asBool();
-    float foreground_inversion = json.result["foreground inversion"].asBool();
-    float noise = json.result["noise"].asFloat();
-    float luma_noise = json.result["luma noise"].asFloat();
 
-
+    white_noise_level     =   json.result["white noise"].asFloat();
+    luma_noise_level      =   json.result["luma noise"].asFloat();
+    rotation_noise_level  =   json.result["rotation noise"].asFloat();
+    shift_noise_level     =   json.result["shift noise"].asInt();
+    mix_threshold         =   json.result["mix threshold"].asFloat();
 
 
 
@@ -60,27 +58,13 @@ DatasetDetection::DatasetDetection(std::string json_file_name)
     {
         unsigned int background_idx = rand()%background.size();
 
-        auto result_true = mix_min( background[background_idx],
-                                    foreground[class_id][item_id],
-                                    threshold,
-                                    alpha,
-                                    background_inversion,
-                                    foreground_inversion,
-                                    noise,
-                                    luma_noise,
-                                    true
-                                    );
+        auto result_true = process_image(   background[background_idx],
+                                            foreground[class_id][item_id],
+                                            true );
 
-        auto result_false = mix_min(    background[background_idx],
-                                        foreground[class_id][item_id],
-                                        threshold,
-                                        alpha,
-                                        background_inversion,
-                                        foreground_inversion,
-                                        noise,
-                                        luma_noise,
-                                        false
-                                    );
+        auto result_false = process_image(  background[background_idx],
+                                            foreground[class_id][item_id],
+                                            false);
 
         sDatasetItem item_true;
 
@@ -149,19 +133,6 @@ void DatasetDetection::load_images(std::vector<std::vector<float>> &result, std:
             if ( (width*downscale != input_width) ||
                  (height*downscale != input_height) )
                  continue;
-            /*
-            if ( (width*downscale != input_width) ||
-                 (height*downscale != input_height) )
-            {
-                std::cout << "loading " << image_file_name  << "\n";
-                std::cout << downscale << "\n";
-                std::cout << width << " " << height << "\n";
-                std::cout << input_height << " " << input_width << "\n";
-                std::cout << "ERROR\n";
-                continue;
-            }
-            */
-
 
             for (unsigned int ch = 0; ch < channels; ch++)
             for (unsigned int y = 0; y < height; y++)
@@ -179,17 +150,51 @@ void DatasetDetection::load_images(std::vector<std::vector<float>> &result, std:
     }
 }
 
-std::vector<float> DatasetDetection::mix_min(   std::vector<float> &background,
-                                                std::vector<float> &foreground,
-                                                float threshold,
-                                                float alpha,
-                                                bool background_inversion,
-                                                bool foreground_inversion,
-                                                float noise,
-                                                float luma_noise,
-                                                bool mix_enabled
-                                            )
+
+std::vector<float> DatasetDetection::process_image( std::vector<float> &background,
+                                                    std::vector<float> &foreground,
+                                                    bool mix_enabled
+                                                 )
 {
+
+    auto background_noised = add_noise(background, white_noise_level, luma_noise_level);
+    auto foreground_noised = add_noise(foreground, white_noise_level, luma_noise_level);
+
+    auto foreground_rotated = rotation_noise(foreground_noised, rotation_noise_level);
+    auto foreground_shifted = shift_noise(foreground_rotated, shift_noise_level);
+
+
+    std::vector<float> mixed;
+
+    if (mix_enabled)
+    {
+        mixed = mix_min(background_noised, foreground_shifted, mix_threshold);
+    }
+    else
+    {
+        mixed = background_noised;
+    }
+
+
+
+    auto result = normalise(mixed);
+
+    std::cout << "\n\ninfo\n";
+    std::cout << width << " " << height << "\n";
+
+    std::cout << "background size" << background.size() << "\n";
+    std::cout << "background noised size" << background_noised.size() << "\n";
+
+    std::cout << "foreground size" << foreground.size() << "\n";
+    std::cout << "foreground noised size" << foreground_noised.size() << "\n";
+    std::cout << "foreground rotated size" << foreground_rotated.size() << "\n";
+    std::cout << "foreground shifted size" << foreground_shifted.size() << "\n";
+
+    std::cout << "mixed size" << mixed.size() << "\n";
+    std::cout << "result size" << result.size() << "\n";
+
+    return result;
+    /*
     unsigned int size = width*height*channels;
     std::vector<float> result(size);
 
@@ -229,7 +234,7 @@ std::vector<float> DatasetDetection::mix_min(   std::vector<float> &background,
 
                 raw+= foreground_luma;
                 result[i] = (1.0 - a)*raw + a*background[i];
-            } 
+            }
         }
     }
 
@@ -268,6 +273,138 @@ std::vector<float> DatasetDetection::mix_min(   std::vector<float> &background,
 
 
     return result;
+    */
+}
+
+
+
+std::vector<float> DatasetDetection::add_noise(std::vector<float> &input, float white_noise, float luma_noise)
+{
+    std::vector<float> result(input.size());
+
+    float luma_noise_ = luma_noise*rndf(-1.0, 1.0);
+
+    for (unsigned int i  = 0; i < input.size(); i++)
+    {
+        result[i] = luma_noise_ + (1.0 - white_noise)*input[i] + white_noise*rndf(-1.0, 1.0);
+    }
+
+    return result;
+}
+
+std::vector<float> DatasetDetection::shift_noise(std::vector<float> &input, int shift_noise)
+{
+    std::vector<float> result(input.size());
+    for (unsigned int i = 0; i < result.size(); i++)
+        result[i] = 0.0;
+
+    int shift_x = rndf(-shift_noise, shift_noise);
+    int shift_y = rndf(-shift_noise, shift_noise);
+
+    for (unsigned int k = 0; k < 3; k++)
+    for (unsigned int y = 0; y < height; y++)
+    for (unsigned int x = 0; x < width; x++)
+    {
+        int y_s = (int)y + shift_y;
+        int x_s = (int)x + shift_x;
+
+        result[to_idx(x, y, k)] = input[to_idx(x_s, y_s, k)];
+    }
+
+    return result;
+}
+
+
+std::vector<float> DatasetDetection::rotation_noise(std::vector<float> &input, float angle_deg)
+{
+    std::vector<float> result(input.size());
+    for (unsigned int i = 0; i < result.size(); i++)
+        result[i] = 0.0;
+
+    float angle_range = angle_deg*3.141592654/180.0;
+
+    float angle = rndf(-angle_range, angle_range);
+
+    float a = cos(angle);
+    float b = -sin(angle);
+    float c = sin(angle);
+    float d = cos(angle);
+
+    for (unsigned int k = 0; k < 3; k++)
+    for (unsigned int y = 0; y < height; y++)
+    for (unsigned int x = 0; x < width; x++)
+    {
+        int x_rot = (int)x*a + (int)y*b;
+        int y_rot = (int)x*c + (int)y*d;
+
+        result[to_idx(x, y, k)] = input[to_idx(x_rot, y_rot, k)];
+    }
+
+    return result;
+}
+
+std::vector<float> DatasetDetection::mix_min(std::vector<float> &background, std::vector<float> &foreground, float threshold)
+{
+    std::vector<float> result(background.size());
+
+    for (unsigned int i  = 0; i < background.size(); i++)
+    {
+        if (foreground[i] < threshold)
+            result[i] = foreground[i];
+        else
+            result[i] = background[i];
+    }
+
+    return result;
+}
+
+
+std::vector<float> DatasetDetection::normalise(std::vector<float> &input)
+{
+    std::vector<float> result(input.size());
+
+    float max = input[0];
+    float min = max;
+
+    for (unsigned int i = 0; i < input.size(); i++)
+    {
+        if (input[i] > max)
+            max = input[i];
+        if (input[i] < min)
+            min = input[i];
+    }
+
+    float k = 0.0;
+    float q = 0.0;
+
+    if (max > min)
+    {
+        k = (1.0 - 0.0)/(max - min);
+        q = 1.0 - k*max;
+    }
+
+    for (unsigned int i = 0; i < input.size(); i++)
+    {
+        result[i] = k*input[i] + q;
+    }
+
+    return result;
+}
+
+
+unsigned int DatasetDetection::to_idx(int x, int y, int ch)
+{
+    if (x < 0)
+        x = 0;
+    if (x > ((int)width-1))
+        x = ((int)width-1);
+
+    if (y < 0)
+        y = 0;
+    if (y > ((int)height-1))
+        y = ((int)height-1);
+
+    return (ch*height + y)*width + x;
 }
 
 
--- libs/dataset_detection.h
+++ libs/dataset_detection.h
@@ -7,10 +7,13 @@
 class DatasetDetection: public DatasetInterface
 {
     private:
-
-
         std::vector<std::vector<float>>              background;
         std::vector<std::vector<std::vector<float>>> foreground;
+
+    private:
+        float white_noise_level, luma_noise_level, rotation_noise_level;
+        int shift_noise_level;
+        float mix_threshold;
 
     public:
         DatasetDetection(std::string json_file_name);
@@ -18,16 +21,19 @@ class DatasetDetection: public DatasetInterface
 
     private:
         void load_images(std::vector<std::vector<float>> &result, std::string path_dir, unsigned int downscale = 1);
-        std::vector<float> mix_min( std::vector<float> &background,
-                                    std::vector<float> &foreground,
-                                    float threshold,
-                                    float alpha,
-                                    bool background_inversion,
-                                    bool foreground_inversion,
-                                    float noise,
-                                    float luma_noise,
-                                    bool mix_enabled);
+        std::vector<float> process_image( std::vector<float> &background,
+                                          std::vector<float> &foreground,
+                                          bool mix_enabled);
 
+    private:
+
+        std::vector<float> add_noise(std::vector<float> &input, float white_noise, float luma_noise);
+        std::vector<float> shift_noise(std::vector<float> &input, int shift_noise);
+        std::vector<float> rotation_noise(std::vector<float> &input, float angle_deg);
+        std::vector<float> mix_min(std::vector<float> &background, std::vector<float> &foreground, float threshold);
+        std::vector<float> normalise(std::vector<float> &input);
+
+        unsigned int to_idx(int x, int y, int ch);
         float rndf(float min = 0.0, float max = 1.0);
 
 };
